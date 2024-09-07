5311241f9cc1b5b1ddc00689acf277a438bee5da
Nikunj Mahawar <90510801+NikeMahawar@users.noreply.github.com>
2024-09-07 15:36:10 +0530

    Update screens.js

--- app/screens.js
+++ app/screens.js
@@ -2,7 +2,7 @@ export const screens = {
     0:{
         title: <span>How does it <span className='text-header-white'>Work</span> <span className='text-header-blue'>?</span></span>,
         description: <h1 className=' text-description-white'>We make it possible in a quick and easy few steps process, takes max 5 mins</h1>,
-        image:'/ss1.png',
+        image:'/Mockup1.png',
         slider: '5%',
         img_layout:'top-1/2 md:-translate-y-1/2',
     },
@@ -12,7 +12,7 @@ export const screens = {
                         <h1 className=' text-description-heading'>Tenant selects the property</h1>
                         <h1 className=' text-description-white'>Tenant selects flexible rent tenure & corresponding amount</h1>
                     </div>,
-        image:'/ss1.png',
+        image:'/Mockup1.png',
         slider: '20%',
         img_layout:'top-1/3 md:scale-[1.5] max-md:scale-[1.3] max-md:translate-y-1/4',
         arrow:'top-1/4 right-[80%]'
@@ -23,7 +23,7 @@ export const screens = {
                         <h1 className=' text-description-white'>Tenant selects the property</h1>
                         <h1 className=' text-description-heading'>Tenant selects flexible rent tenure & corresponding amount</h1>
                     </div>,
-        image: '/ss2.png',
+        image: '/Mockup2.png',
         slider: '30%',
         img_layout:'top-[20%] md:scale-[1.4]',
         arrow:'top-[40%] right-[40%]',
@@ -39,7 +39,7 @@ export const screens = {
                             <li>3 months salary cover</li>
                         </ul>
                     </div>,
-        image:'/ss3.png',
+        image:'/Mockup3.png',
         slider: '60%',
         img_layout:'md:-translate-y-[10%] md:scale-[1.4]',
         arrow:'top-[55%] right-[75%]',
@@ -48,7 +48,7 @@ export const screens = {
     4:{
         title:<span>Step <span className='text-header-white'>3</span></span>,
         description: <h1 className=' text-description-heading'>Smooth Onboarding for the Tenant begins</h1>,
-        image:'/ss4.png',
+        image:'/Mockup4.png',
         slider: '80%',
         img_layout:'top-[5%]',
         arrow:'top-[52%] right-[70%]',
@@ -63,7 +63,7 @@ export const screens = {
                             <li>Zero cost EMI = Monthly Rent</li>
                         </ul>
                     </div>,
-        image:'/ss5.png',
+        image:'/Mockup5.png',
         slider: '100%',
         img_layout:'top-[9%]',
         arrow:'top-[58%] right-[70%]',
