c302d54608ffa525982231bab6a8adabe177332d
Nikunj Mahawar <90510801+NikeMahawar@users.noreply.github.com>
2024-09-07 15:35:36 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
+/* I have added comments for the judge from the circlepe so that you can understand the code structure*/
 "use client"
 import { useEffect,useState } from 'react'
 import { screens } from './screens'
@@ -22,10 +22,14 @@ function Home() {
 
   return (
     <section className="w-full flex flex-col md:flex-row-reverse md:items-center md:justify-center h-screen bg-black relative overflow-hidden ">
-      {/* bg gradient */}
+      {/* background */}
+
+
       <div className="top-[-190px] left-[-400px] w-[1103px] h-[1017px] hidden md:flex absolute bg-[radial-gradient(35.19%_35.19%_at_50%_50%,rgba(59,149,255,0.17)_0%,rgba(28,106,197,0)_100%)] overflow-clip "></div>
 
-        {/* top slider for small screens */}
+        {/* meter for phone screens */}
+
+
         <div className='min-h-[60px] md:hidden flex items-center justify-center p-2'>
           <div className='w-full max-w-[400px] bg-slider-bg h-[3px] rounded-full'> 
             <div className={`${index==5?"bg-slider-green":"bg-slider-blue"} h-full relative transition-all ease-out rounded-full`} style={{width:screens[index].slider}}>
@@ -33,7 +37,12 @@ function Home() {
             </div>
           </div>
         </div>
-        {/* right slider for larger screens */}
+
+
+
+        {/* meter for big or laptop screens */}
+
+
         <div className='h-full w-full max-w-[300px] max-md:hidden flex items-center justify-center p-2'>
           <div className='h-full max-h-[400px] bg-slider-bg w-[3px] rounded-full'> 
             <div className={`${index==5?"bg-slider-green":"bg-slider-blue"} h-full relative transition-all ease-out rounded-full`} style={{height:screens[index].slider}}>
@@ -41,9 +50,12 @@ function Home() {
             </div>
           </div>
         </div>
-        {/* page content */}
+
+
+        {/* Content inside the page */}
+
+
         <div className='flex flex-col md:flex-row w-full md:w-[calc(100%-300px)]  h-[calc(100%-60px)] md:h-full gap-y-5 md:gap-x-10 items-center justify-center md:items-center'>
-            {/* text content */}
             <div className='flex felx-col w-full max-w-[380px] md:max-w-[422px]'>
               <div className='flex items-start justify-center flex-col gap-y-[16px] md:gap-y-[48px] p-4 py-2  w-full'>
                 <h1 className='text-header md:text-header-lg md:leading-[64px] text-header-gray font-medium max-w-[283px]'>{screens[index].title}</h1>
@@ -53,7 +65,12 @@ function Home() {
                 </div>
               </div>
             </div>
-            {/* image and arrow*/}
+
+
+
+            {/* Moving arrow */}
+
+
             <div className=' h-[calc(100%-206px)] w-fit md:h-full relative md:max-h-screen md:w-fit min-w-[300px] max-w-[340px] flex-shrink'>
                 <img fill src={screens[index].image} alt="onboarding image" className={`h-full max-md:mx-auto md:w-full md:h-auto flex-shrink md:flex-shrink-0 md:absolute transition-all duration-300 ease-out ${screens[index].img_layout}`} />
                 {screens[index]?.arrow && <img fill src='/arrow.png' alt="indication arrow" className={`max-md:hidden w-full h-auto absolute transition-all duration-300 ${screens[index].arrow}`} style={screens[index]?.arrow_additional||{}} />}
